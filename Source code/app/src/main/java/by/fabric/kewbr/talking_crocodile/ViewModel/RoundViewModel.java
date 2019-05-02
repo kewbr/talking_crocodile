3bc8734c09ee4bbfe508aa7f2d0bffabc8e76908
Yan Khamutouski <y.khamutouski@gmail.com>
2019-05-02 09:17:28 +0300

    fix for count bug

--- Source code/app/src/main/java/by/fabric/kewbr/talking_crocodile/ViewModel/RoundViewModel.java
+++ Source code/app/src/main/java/by/fabric/kewbr/talking_crocodile/ViewModel/RoundViewModel.java
@@ -11,6 +11,9 @@ public class RoundViewModel {
 
     private Realm databaseInstance;
     private List<WordStatusModel> words = new ArrayList<>();
+    private List<WordStatusModel> copyOfWords = new ArrayList<>();
+
+    private List <ProgressModel> teamsPlaying = new ArrayList<>();
 
     public RoundViewModel(int roundNumber) {
 
@@ -22,6 +25,12 @@ public class RoundViewModel {
                 .equalTo("roundNumber", Long.valueOf(roundNumber))
                 .findAll();
         words.addAll(databaseInstance.copyFromRealm(result));
+        copyOfWords.addAll(databaseInstance.copyFromRealm(result));
+
+        RealmResults<ProgressModel> teams = databaseInstace
+                .where(ProgressModel.class)
+                .findAll();
+        teamsPlaying.addAll(databaseInstance.copyFromRealm(teams));
     }
 
     public List<WordStatusModel> getWords() {
@@ -30,9 +39,43 @@ public class RoundViewModel {
 
     public void writeBack() {
 
+        //ахтунг чек
+
+        int[] delta = new int[this.teamsPlaying.size()];
+
+        for (int i = 0; i < this.teamsPlaying.size(); i++) {
+            delta[i] = 0;
+        }
+
+        for (int i = 0; i < teamsPlaying.size(); i++) {
+            for (int j = 0; j < words.size(); j++) {
+
+                if (words.get(j).getGuessed() == false && copyOfWords.get(j).getGuessed() == true) {
+
+                    delta[i] += -1;             //тут прохэндилить нужно ли отнимать
+                                                //это в настройках чекать по всей игре
+                                                //ахтунг!!!
+                }
+
+                if (words.get(j).getGuessed() == true && copyOfWords.get(j).getGuessed() == false) {
+
+                    delta[i] += 1;
+                }
+
+            }
+        }
+
+        for (int i = 0; i < teamsPlaying.size(); i++) {
+            int oldGuessedCount = teamsPlaying.get(i).getGuessedCount();
+            oldGuessedCount += delta[i];
+        }
+
+        //ахтунг чек
+
         databaseInstance.beginTransaction();
 
         databaseInstance.copyToRealm(words);
+        databaseInstance.copyToRealm(teamsPlaying);     //ахтунг чек
 
         databaseInstance.commitTransaction();
 
